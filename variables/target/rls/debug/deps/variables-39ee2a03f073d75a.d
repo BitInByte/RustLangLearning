3b964ad67790eee69a1da0d36b923291eee6a635
BitInByte <bitinbyte@outlook.com>
2021-12-22 20:47:13 +0000

    Completed Section

--- variables/target/rls/debug/deps/variables-39ee2a03f073d75a.d
+++ variables/target/rls/debug/deps/variables-39ee2a03f073d75a.d
@@ -1,5 +1,5 @@
-/Users/foxy/Documents/data/Learnings/variables/target/rls/debug/deps/variables-39ee2a03f073d75a.rmeta: src/main.rs
+/Users/foxy/Documents/data/Learnings/Rust/variables/target/rls/debug/deps/variables-39ee2a03f073d75a.rmeta: src/main.rs
 
-/Users/foxy/Documents/data/Learnings/variables/target/rls/debug/deps/variables-39ee2a03f073d75a.d: src/main.rs
+/Users/foxy/Documents/data/Learnings/Rust/variables/target/rls/debug/deps/variables-39ee2a03f073d75a.d: src/main.rs
 
 src/main.rs:
--- variables/target/rls/debug/deps/variables-b705979a20cc893a.d
+++ variables/target/rls/debug/deps/variables-b705979a20cc893a.d
@@ -1,5 +1,5 @@
-/Users/foxy/Documents/data/Learnings/variables/target/rls/debug/deps/variables-b705979a20cc893a.rmeta: src/main.rs
+/Users/foxy/Documents/data/Learnings/Rust/variables/target/rls/debug/deps/variables-b705979a20cc893a.rmeta: src/main.rs
 
-/Users/foxy/Documents/data/Learnings/variables/target/rls/debug/deps/variables-b705979a20cc893a.d: src/main.rs
+/Users/foxy/Documents/data/Learnings/Rust/variables/target/rls/debug/deps/variables-b705979a20cc893a.d: src/main.rs
 
 src/main.rs:
